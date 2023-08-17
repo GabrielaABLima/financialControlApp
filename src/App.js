8c1931a209c22c929ed62999c4db032f039eada2
Gabriela Lima <gabriela.andrade.bueno.lima@gmail.com>
2023-08-17 12:50:19 -0300

    Fixed date presentation

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ function App() {
 
   const expenses = [
     { id: 0, title: "Car Insurance", amount: 294.67, date: new Date(2023, 7, 11) }, // Mês 7 representa Agosto
-    { id: 1, title: "Groceries", amount: 150.25, date: new Date(2023, 7, 10) },
-    { id: 2, title: "Rent", amount: 1200.0, date: new Date(2023, 7, 1) },
-    { id: 3, title: "Utilities", amount: 200.0, date: new Date(2023, 7, 5) },
-    { id: 4, title: "Entertainment", amount: 50.0, date: new Date(2023, 7, 8) },
-    { id: 5, title: "Healthcare", amount: 80.5, date: new Date(2023, 7, 3) }
+    { id: 1, title: "Groceries", amount: 150.25, date: new Date(2023, 4, 10) },
+    { id: 2, title: "Rent", amount: 1200.0, date: new Date(2023, 3, 1) },
+    { id: 3, title: "Utilities", amount: 200.0, date: new Date(2023, 2, 5) },
+    { id: 4, title: "Entertainment", amount: 50.0, date: new Date(2023, 12, 8) },
+    { id: 5, title: "Healthcare", amount: 80.5, date: new Date(2023, 10, 3) }
   ];
   return (
     <div>
